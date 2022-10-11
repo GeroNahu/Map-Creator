1afb48fd44f559e9c8a1dfa764e18ca6c81434c8
Geronimo Andrada <aikanaropal10@gmail.com>
2022-10-11 13:59:07 -0300

    First commit

---
+++ mapcreator/src/constants/themes.js
@@ -0,0 +1,29 @@
+export const themes = {
+  medieval: [
+    `url(${require(`../Images/Medievales/0.jpg`)})`,
+    `url(${require(`../Images/Medievales/1.jpg`)})`,
+    `url(${require(`../Images/Medievales/2.jpg`)})`,
+    `url(${require(`../Images/Medievales/3.jpg`)})`,
+    `url(${require(`../Images/Medievales/4.jpg`)})`,
+    `url(${require(`../Images/Medievales/3.png`)})`,
+    `url(${require(`../Images/Medievales/4.png`)})`,
+  ],
+  futurist: [
+    `url(${require(`../Images/Futuristas/0.jpg`)})`,
+    `url(${require(`../Images/Futuristas/1.jpg`)})`,
+    `url(${require(`../Images/Futuristas/2.jpg`)})`,
+  ],
+  contemporany: [
+    `url(${require(`../Images/Contemporaneo/0.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/1.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/2.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/3.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/0.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/1.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/2.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/3.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/0.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/1.jpg`)})`,
+    `url(${require(`../Images/Contemporaneo/2.jpg`)})`,
+  ],
+};
