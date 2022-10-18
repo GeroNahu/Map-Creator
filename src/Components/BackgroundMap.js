81a83e176913f66954c2c46e853e30760d2c15fb
aikanaro <aikanaropal10@gmail.com>
2022-10-18 20:21:24 -0300

    Refactors BackgroundMap component and adds handleClick

--- src/Components/BackgroundMap.js
+++ src/Components/BackgroundMap.js
@@ -4,13 +4,54 @@ import Tile from "./Tile";
 
 import "../Styles/backgroundMap.css";
 
-const BackgroundMap = ({ selected, layer }) => {
-  const tilesFor = [];
-  for (let i = 0; i < 9; i++) {
-    tilesFor.push(<Tile selected={selected} layerNumber={layer} />);
-  }
-  console.log(tilesFor);
-  return <div className="backgroundMap">{tilesFor}</div>;
+const BackgroundMap = ({ selected, selectedLayer, width, height }) => {
+  const [mapa, setMapa] = React.useState([{}]);
+  React.useEffect(() => {
+    const newMap = [];
+    for (let i = 0; i < width * height; i++) {
+      newMap.push({
+        x: i + 1 - Math.floor(i / width) * width,
+        y: Math.ceil((i + 1) / width),
+        layers: ["", "", ""],
+      });
+    }
+    setMapa(newMap);
+  }, [width, height]);
+
+  const handleClick = (x, y) => {
+    setMapa(
+      mapa.map((tile) => {
+        const newTile = { ...tile };
+        if (tile.x === x && tile.y === y) {
+          newTile.layers[selectedLayer] = selected;
+        }
+        return newTile;
+      })
+    );
+  };
+
+  return (
+    <div className="backgroundMapContainer">
+      <div
+        className="backgroundMap"
+        style={{
+          gridTemplateColumns: `repeat(${width}, minmax(134px, 1fr))`,
+          gridTemplateRows: `repeat(${height}, minmax(134px, 1fr))`,
+          gridAutoRows: "minmax(134px, 1fr)",
+        }}
+      >
+        {mapa.map((tile) => {
+          return (
+            <Tile
+              {...tile}
+              key={`tile_${tile.x}_${tile.y}`}
+              onClick={() => handleClick(tile.x, tile.y)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default BackgroundMap;
