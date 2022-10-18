59f2d68f428ecd19f394b9d6c83064f110423b73
aikanaro <aikanaropal10@gmail.com>
2022-10-18 13:13:13 -0300

    Create globalStyles file

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,36 +1,20 @@
 import React from "react";
 
-const Tile = ({ selected, layerNumber = 1 }) => {
-  const [images, setImages] = React.useState({
-    layer1: "",
-    layer2: "",
-    layer3: "",
-  });
-  const handleClick = () => {
-    const updated = { ...images };
-    const layer = "layer" + layerNumber;
-    updated[layer] = selected;
-    console.log(updated);
-    setImages(updated);
-  };
-
+const Tile = ({ x, y, layers = [], ...rest }) => {
   return (
-    <div className="tile" onClick={handleClick}>
-      <div
-        className="layer"
-        style={{
-          backgroundImage: images.layer1,
-          backgroundSize: "contain",
-        }}
-      />
-      <div
-        className="layer"
-        style={{ backgroundImage: images.layer2, backgroundSize: "contain" }}
-      />
-      <div
-        className="layer"
-        style={{ backgroundImage: images.layer3, backgroundSize: "contain" }}
-      />
+    <div className="tile" {...rest}>
+      {layers?.map((layer, index) => {
+        return (
+          <div
+            className="layer"
+            key={`tile_${x}_${y}_layer_${index}`}
+            style={{
+              backgroundImage: layer,
+              backgroundSize: "contain",
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
