0d7db181804f367c52013361113d5d69825c0176
aikanaro <aikanaropal10@gmail.com>
2022-10-18 13:07:04 -0300

    Swap BackgroundMap for SectionContainer

--- src/Components/SectionContainer.js
+++ src/Components/SectionContainer.js
@@ -1,23 +1,27 @@
 import React from "react";
-import BackgroundMap from "./BackgroundMap";
+import MapSection from "./MapSection";
 import ToolContainer from "./ToolsContainer";
 import "../Styles/sectionContainer.css";
 
 const SectionContainer = () => {
   const [selectedImage, setSelectedImage] = React.useState("");
-  const [layer, setLayer] = React.useState(1);
+  const [selectedLayer, setSelectedLayer] = React.useState(1);
 
   return (
     <div className="container">
       <div className="divBackgorundMapContainer">
-        <BackgroundMap selected={selectedImage} layer={layer} />
+        <MapSection
+          selected={selectedImage}
+          selectedLayer={selectedLayer}
+          setSelectedLayer={setSelectedLayer}
+        />
       </div>
       <div className="divToolsContainer">
         <ToolContainer
           onImageSelect={setSelectedImage}
           selectedImage={selectedImage}
-          onLayerSelect={setLayer}
-          selectedLayer={layer}
+          onLayerSelect={setSelectedLayer}
+          selectedLayer={selectedLayer}
         />
       </div>
     </div>
