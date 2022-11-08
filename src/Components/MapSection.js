94f4ebe2e23c3422fa958ab585a5b163d017afda
aikanaro <aikanaropal10@gmail.com>
2022-11-08 11:37:47 -0300

    Consumes the context provided and insert inline styles to apply theme dynamic changes

--- src/Components/MapSection.js
+++ src/Components/MapSection.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import BackgroundMap from "./BackgroundMap";
 import ZoomSelector from "./ZoomSelector";
 import GridSizeSelector from "./GridSizeSelector";
+import ThemesContext from "../constants/ThemesContext";
+
 import "../Styles/globalStyles.css";
 import "../Styles/mapSection.css";
 
@@ -9,14 +12,33 @@ const MapSection = ({ selected, selectedLayer, setSelectedLayer, layer }) => {
   const [width, setWidth] = React.useState(1);
   const [height, setHeight] = React.useState(1);
   const [mapSize, setMapSize] = React.useState(100);
+  const { theme } = React.useContext(ThemesContext);
 
   return (
-    <section className="divBackgorundMapContainer">
+    <section
+      className="divBackgorundMapContainer"
+      style={{
+        backgroundColor: theme.MAP_SECTION_BACKGROUND,
+      }}
+    >
       <div className="mapHeader">
         <div className="titleContainer">
-          <h2 className="h2">Map</h2>
+          <h2
+            className="h2"
+            style={{
+              color: theme.TITLES,
+            }}
+          >
+            Map
+          </h2>
         </div>
-        <div className="mapHeaderSelectors">
+        <div
+          className="mapSelectorsContainer"
+          style={{
+            backgroundColor: theme.MAP_SELECTORS_CONTAINER_BACKGROUND,
+            border: `solid ${theme.MAP_SELECTORS_CONTAINER_BORDER} 3px`,
+          }}
+        >
           <GridSizeSelector
             width={width}
             height={height}
