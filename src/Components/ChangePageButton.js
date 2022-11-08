f63d9f60699ec817e27d07992c0c8d9fb81da286
aikanaro <aikanaropal10@gmail.com>
2022-11-08 11:55:38 -0300

    Consumes the context provided and insert inline styles to apply theme dynamic changes

--- src/Components/ChangePageButton.js
+++ src/Components/ChangePageButton.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "../Styles/changePageButton.css";
+import ThemeContext from "../constants/ThemesContext";
 
 const ChangePageButton = ({ buttonLabel, setImages }) => {
+  const { theme } = React.useContext(ThemeContext);
+
   return (
-    <button className="changePageButton" onClick={setImages}>
+    <button
+      className="changePageButton"
+      onClick={setImages}
+      style={{
+        backgroundColor: theme.THEME_BUTTONS_BACKGROUND,
+        border: `solid ${theme.THEME_BUTTONS_BORDER} 3px`,
+        color: theme.TEXT_PRIMARY,
+      }}
+    >
       {buttonLabel}
     </button>
   );
