1b6196bc21b5b8cca8d62018b302461df4ae4654
aikanaro <aikanaropal10@gmail.com>
2022-11-08 11:53:29 -0300

    Adds HandleTheme component to change themes

---
+++ src/Components/HandleTheme.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ThemesContext from "../constants/ThemesContext";
+
+import "../Styles/globalStyles.css";
+import "../Styles/HandleTheme.css";
+
+const HandleTheme = () => {
+  const { theme, setTheme } = React.useContext(ThemesContext);
+  const [onOff, setOnOff] = React.useState(false);
+
+  return (
+    <div
+      className="toggle"
+      onClick={() => {
+        setOnOff(!onOff);
+        setTheme(onOff ? "light" : "dark");
+      }}
+      style={{
+        backgroundColor: theme.TOOGLE_CONTAINER_BACKGROUND,
+        border: `solid ${theme.TOOGLE_CONTAINER_BORDER} 3px`,
+      }}
+    >
+      <div
+        className={`toggleBar ${onOff ? "on" : ""}`}
+        style={{ backgroundColor: theme.TOOGLE_BAR_BACKGROUND }}
+      >
+        <div
+          className={`toggleCircle ${onOff ? "on" : ""}`}
+          style={{ backgroundColor: theme.TOOGLE_CIRCLE_BACKGROUND }}
+        />
+      </div>
+      <h5 className="h5" style={{ color: theme.TEXT_PRIMARY }}>
+        Nocturne Mode
+      </h5>
+    </div>
+  );
+};
+
+export default HandleTheme;
