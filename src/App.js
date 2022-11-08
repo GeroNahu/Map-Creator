8886ae10843b88aff6e49738bacb39f5c3e712da
aikanaro <aikanaropal10@gmail.com>
2022-11-08 11:59:19 -0300

    Moves header to Header Component and provided the context to change the themes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import SectionContainer from "./Components/SectionContainer";
+import Header from "./Components/Header";
+import { ThemesProvider } from "./constants/ThemesContext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="h1">Map Creator</h1>
-      </header>
-      <SectionContainer />
+      <ThemesProvider>
+        <Header />
+        <SectionContainer />
+      </ThemesProvider>
     </div>
   );
 }
