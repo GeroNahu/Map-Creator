795c0dc4dc2d7fa6a2f76dd6352835fd99a533c9
aikanaro <aikanaropal10@gmail.com>
2022-12-16 15:34:00 -0300

    Adds a new file contain the application cursors object

---
+++ src/constants/AppCursors.js
@@ -0,0 +1,6 @@
+const cursors = {
+  pen: `url(${require("../Images/Icons/pen.gif")}), default`,
+  ereaser: `url(${require("../Images/Icons/trash can.gif")}), default`,
+  eyedropper: `url(${require("../Images/Icons/eyedropper.gif")}), default`,
+};
+export default cursors;
