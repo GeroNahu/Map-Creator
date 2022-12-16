e2daa53fb1a502ace4ca803cabf7740a61e7ccda
aikanaro <aikanaropal10@gmail.com>
2022-12-16 16:53:46 -0300

    Creates a new state variable selectedCategor

--- src/Components/ChangePageButtons.js
+++ src/Components/ChangePageButtons.js
@@ -1,17 +1,21 @@
 import React from "react";
-import ChangePageButton from "./ChangePageButton";
+import CategoryButton from "./ChangepageButton";
 
 import "../Styles/changePageButton.css";
 
 const ChangePageButtons = ({ themes, setImages }) => {
+  const [selectedCategory, setSelectedCategory] = React.useState("medieval");
+
   const buttons = themes.map((theme) => (
-    <ChangePageButton
+    <CategoryButton
       buttonLabel={theme}
-      setImages={() => setImages(theme)}
+      setImages={setImages}
       key={`theme_${theme}`}
+      setSelectedCategory={setSelectedCategory}
+      selectedCategory={theme === selectedCategory}
     />
   ));
-  return <div className="themeButtons">{buttons}</div>;
+  return <div className="categoryTilesButtonsContainer">{buttons}</div>;
 };
 
 export default ChangePageButtons;
