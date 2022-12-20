4e8b8ea4d7792ec491825c3df2b79b9989224247
aikanaro <aikanaropal10@gmail.com>
2022-12-20 18:08:53 -0300

    Adds MapContext

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import SectionContainer from "./Components/SectionContainer";
 import Header from "./Components/Header";
 import { ThemesProvider } from "./Contexts/ThemesContext";
 import { CursorsProvider } from "./Contexts/CursorsContext";
+import { MapProvider } from "./Contexts/MapContext";
 
 function App() {
   return (
     <div className="App">
       <ThemesProvider>
         <CursorsProvider>
-          <Header />
+          <MapProvider>
+            <Header />
+          </MapProvider>
           <SectionContainer />
         </CursorsProvider>
       </ThemesProvider>
