10c50ce2fd76cb993981441a9fff8fc1a6c8ab13
aikanaro <aikanaropal10@gmail.com>
2022-12-20 22:04:19 -0300

    Refactors map to big objet

--- src/Contexts/MapContext.js
+++ src/Contexts/MapContext.js
@@ -3,7 +3,13 @@ import React from "react";
 const MapContext = React.createContext();
 
 const MapProvider = ({ children }) => {
-  const [map, setMap] = React.useState("");
+  const defaultTile = { x: 0, y: 0, layers: [""] };
+  const [map, setMap] = React.useState({
+    title: "",
+    layers: [""],
+    Tiles: [defaultTile],
+    metaData: {},
+  });
 
   const data = { map, setMap };
 
