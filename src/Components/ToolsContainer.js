420f0961f4c0bd005d19a265cc32591b51908476
aikanaro <aikanaropal10@gmail.com>
2022-12-20 22:01:42 -0300

    Makes the buttons dinamic

--- src/Components/ToolsContainer.js
+++ src/Components/ToolsContainer.js
@@ -5,12 +5,36 @@ import CursorsContext from "../Contexts/CursorsContext";
 
 import "../Styles/toolsCointainer.css";
 
-const ToolsContainer = ({ handleToolChange }) => {
+const ToolsContainer = ({ handleToolChange, toolsList, selectedTool }) => {
   const { theme } = React.useContext(ThemeContext);
   const { setCursor } = React.useContext(CursorsContext);
-
-  const [buttonClass, setButtonClass] = React.useState("penOn");
-
+  const renderButtons = () => {
+    return toolsList.map((tool, index) => {
+      return (
+        <div
+          className={`backgroundButton ${
+            selectedTool === tool ? "backgroundButtonOn" : ""
+          }`}
+          key={`tool_${tool}_${index}`}
+        >
+          <button
+            className={`toolButton ${
+              selectedTool === tool ? "toolButtonOn" : ""
+            }`}
+            style={{
+              backgroundImage: `url(${require(`../Images/Icons/${tool}.png`)})`,
+              backgroundSize: "30px",
+              backgroundPosition: "center",
+            }}
+            onClick={() => {
+              handleToolChange(tool);
+              setCursor(tool);
+            }}
+          />
+        </div>
+      );
+    });
+  };
   return (
     <section className="toolsContainer">
       <div
@@ -20,69 +44,7 @@ const ToolsContainer = ({ handleToolChange }) => {
           border: `solid ${theme.SELECTED_TOOL_BORDER} 3px`,
         }}
       >
-        <div
-          className={`backgroundButton ${
-            buttonClass === "penOn" ? "backgroundButtonOn" : ""
-          }`}
-        >
-          <button
-            className={`toolButton ${
-              buttonClass === "penOn" ? "toolButtonOn" : ""
-            }`}
-            style={{
-              backgroundImage: `url(${require(`../Images/Icons/pen.png`)})`,
-              backgroundSize: "31px",
-              backgroundPosition: "center",
-            }}
-            onClick={() => {
-              handleToolChange("pen");
-              setCursor("pen");
-              setButtonClass("penOn");
-            }}
-          />
-        </div>
-        <div
-          className={`backgroundButton ${
-            buttonClass === "ereaserOn" ? "backgroundButtonOn" : ""
-          }`}
-        >
-          <button
-            className={`toolButton ${
-              buttonClass === "ereaserOn" ? "toolButtonOn" : ""
-            }`}
-            style={{
-              backgroundImage: `url(${require(`../Images/Icons/trash can.png`)})`,
-              backgroundSize: "32px",
-              backgroundPosition: "center",
-            }}
-            onClick={() => {
-              handleToolChange("ereaser");
-              setCursor("ereaser");
-              setButtonClass("ereaserOn");
-            }}
-          />
-        </div>
-        <div
-          className={`backgroundButton ${
-            buttonClass === "eyedropperOn" ? "backgroundButtonOn" : ""
-          }`}
-        >
-          <button
-            className={`toolButton ${
-              buttonClass === "eyedropperOn" ? "toolButtonOn" : ""
-            }`}
-            style={{
-              backgroundImage: `url(${require(`../Images/Icons/eyedropper.png`)})`,
-              backgroundSize: "30px",
-              backgroundPosition: "center",
-            }}
-            onClick={() => {
-              handleToolChange("dropper");
-              setCursor("eyedropper");
-              setButtonClass("eyedropperOn");
-            }}
-          />
-        </div>
+        {renderButtons()}
       </div>
     </section>
   );
