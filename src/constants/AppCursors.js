fcf5081552a7752c38a2d7cd596502fab9dc29c4
aikanaro <aikanaropal10@gmail.com>
2022-12-20 22:03:35 -0300

    Renames url

--- src/constants/AppCursors.js
+++ src/constants/AppCursors.js
@@ -1,6 +1,6 @@
 const cursors = {
   pen: `url(${require("../Images/Icons/pen.gif")}) 0 35, default`,
-  ereaser: `url(${require("../Images/Icons/trash can.gif")}) 17 17, default`,
+  eraser: `url(${require("../Images/Icons/eraser.gif")}) 17 17, default`,
   eyedropper: `url(${require("../Images/Icons/eyedropper.gif")}) 0 35, default`,
 };
 export default cursors;
