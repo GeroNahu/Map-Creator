b579a5a9d049db8e1b3800a78de3fc9883c2d767
aikanaro <aikanaropal10@gmail.com>
2022-12-24 20:24:13 -0300

    Removes comented code

--- src/Components/VisibilityLayers.js
+++ src/Components/VisibilityLayers.js
@@ -31,34 +31,6 @@ const VisibilityLayers = ({ visibleLayers, handleSetVisibleLayers }) => {
           </div>
         );
       })}
-      {/* <input
-        name="layer2"
-        className="checkBoxLayer"
-        id="layer2"
-        type="checkbox"
-        defaultChecked={`${visibleLayers[1]}`}
-        value="true"
-        onChange={(e) => {
-          return handleSetVisibleLayers(1, e.target.checked);
-        }}
-      />
-      <label htmlFor="layer2" className="visibleLayerLabel">
-        Layer 2
-      </label>
-      <input
-        name="layer3"
-        className="checkBoxLayer"
-        id="layer3"
-        type="checkbox"
-        defaultChecked={`${visibleLayers[2]}`}
-        value="true"
-        onChange={(e) => {
-          return handleSetVisibleLayers(2, e.target.checked);
-        }}
-      />
-      <label htmlFor="layer3" className="visibleLayerLabel">
-        Layer 3
-      </label> */}
     </div>
   );
 };
