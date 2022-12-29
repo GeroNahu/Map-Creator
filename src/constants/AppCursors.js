53b340eb8dd150a2c93c6a0bef0fb6af2b01b5f4
aikanaro <aikanaropal10@gmail.com>
2022-12-29 16:07:55 -0300

    Adds new cursors

--- src/constants/AppCursors.js
+++ src/constants/AppCursors.js
@@ -1,6 +1,9 @@
 const cursors = {
   pen: `url(${require("../Images/Icons/pen.gif")}) 0 35, default`,
   eraser: `url(${require("../Images/Icons/eraser.gif")}) 17 17, default`,
+  eraserLayer: `url(${require("../Images/Icons/eraserLayer.gif")}) 17 17, default`,
   eyedropper: `url(${require("../Images/Icons/eyedropper.gif")}) 0 35, default`,
+  paintBucket: `url(${require("../Images/Icons/paintBucket.gif")}) 35 35, default`,
+  dragAndDrop: `url(${require("../Images/Icons/dragAndDrop.gif")}) 0 35, default`,
 };
 export default cursors;
