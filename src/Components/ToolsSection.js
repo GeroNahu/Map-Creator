7b1f6ce7a07e040c0de3a595ece543b7d11cef63
aikanaro <aikanaropal10@gmail.com>
2023-01-02 11:13:11 -0300

    Fixes renameLayer input defaultValue

--- src/Components/ToolsSection.js
+++ src/Components/ToolsSection.js
@@ -180,7 +180,7 @@ const ToolsSection = ({
             +
           </button>
           <input
-            defaultValue={map.layers[selectedLayer]}
+            defaultValue={map?.layers?.[selectedLayer]}
             type="text"
             className="inputRenameLayer"
             name="layerName"
