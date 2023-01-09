40312e6bd7beebe6258778bae9fe52a00976f491
aikanaro <aikanaropal10@gmail.com>
2023-01-08 23:44:53 -0300

    Creates app languages file

---
+++ src/constants/AppLanguage.js
@@ -0,0 +1,49 @@
+const languages = {
+  english: {
+    APP_HEADER_TITLE: "Map Creator",
+    HANDLE_THEME_LABEL: "Dark Mode",
+    DEFAULT_MAP_TITLE: "Untitled",
+    GRID_SELECTOR_COLUMNS_LABEL: "Columns",
+    GRID_SELECTOR_ROWS_LABEL: "Rows",
+    LAYER_DEFAULT_NAME: "Layer",
+    RENAME_LAYERS_SUBMIT_BUTTON: "Submit",
+  },
+  español: {
+    APP_HEADER_TITLE: "Creador de Mapa",
+    HANDLE_THEME_LABEL: "Modo Oscuro",
+    DEFAULT_MAP_TITLE: "Sin Título",
+    GRID_SELECTOR_COLUMNS_LABEL: "Columnas",
+    GRID_SELECTOR_ROWS_LABEL: "Filas",
+    LAYER_DEFAULT_NAME: "Capa",
+    RENAME_LAYERS_SUBMIT_BUTTON: "Aceptar",
+  },
+  Italiano: {
+    APP_HEADER_TITLE: "Creatore di Mappe",
+    HANDLE_THEME_LABEL: "Modalità scura",
+    DEFAULT_MAP_TITLE: "Senza titolo",
+    GRID_SELECTOR_COLUMNS_LABEL: "Colonne",
+    GRID_SELECTOR_ROWS_LABEL: "Righe",
+    LAYER_DEFAULT_NAME: "Strato",
+    RENAME_LAYERS_SUBMIT_BUTTON: "Invia",
+  },
+  Deutsch: {
+    APP_HEADER_TITLE: "Kartenersteller",
+    HANDLE_THEME_LABEL: "Dunkler Modus",
+    DEFAULT_MAP_TITLE: "Ohne Titel",
+    GRID_SELECTOR_COLUMNS_LABEL: "Säulen",
+    GRID_SELECTOR_ROWS_LABEL: "Reihen",
+    LAYER_DEFAULT_NAME: "Schicht",
+    RENAME_LAYERS_SUBMIT_BUTTON: "Einreichen",
+  },
+  Esperanto: {
+    APP_HEADER_TITLE: "Map Kreinto",
+    HANDLE_THEME_LABEL: "Malhela Reĝimo",
+    DEFAULT_MAP_TITLE: "Sentitola",
+    GRID_SELECTOR_COLUMNS_LABEL: "Kolumnoj",
+    GRID_SELECTOR_ROWS_LABEL: "Vicoj",
+    LAYER_DEFAULT_NAME: "Tavolo",
+    RENAME_LAYERS_SUBMIT_BUTTON: "Submeti",
+  },
+};
+
+export default languages;
