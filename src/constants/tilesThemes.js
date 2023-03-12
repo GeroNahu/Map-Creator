dea331f54ddc5b3ec6876e0e1224440c9faf7f69
aikanaro <aikanaropal10@gmail.com>
2023-03-12 07:41:30 -0300

    Adds and modify new tiles to categories.

--- src/constants/tilesThemes.js
+++ src/constants/tilesThemes.js
@@ -3,17 +3,22 @@ for (let i = 1; i <= 250; i++) {
   exile.push(`url(${require(`../Images/Exile/tile (${i}).png`)})`);
 }
 const medievals = [];
-for (let i = 1; i <= 25; i++) {
-  medievals.push(`url(${require(`../Images/Medievales/medieval (${i}).jpg`)})`);
+for (let i = 1; i <= 766; i++) {
+  medievals.push(`url(${require(`../Images/Medievales/medieval${i}.png`)})`);
 }
 const futurist = [];
-for (let i = 1; i <= 3; i++) {
-  futurist.push(`url(${require(`../Images/Futuristas/futurist (${i}).jpg`)})`);
+for (let i = 1; i <= 29; i++) {
+  futurist.push(`url(${require(`../Images/Futuristas/futurist${i}.png`)})`);
 }
 const contemporany = [];
 for (let i = 1; i <= 48; i++) {
   contemporany.push(
-    `url(${require(`../Images/Contemporaneo/contemporany (${i}).jpg`)})`
+    `url(${require(`../Images/Contemporaneo/contemporany${i}.jpg`)})`
+  );
+}
+for (let i = 1; i <= 4; i++) {
+  contemporany.push(
+    `url(${require(`../Images/Contemporaneo/contemporany${i}.png`)})`
   );
 }
 export const tilesThemes = {
