4aaa0ddf10edd56b3f6cd99353f1f8214c825ee9
aikanaro <aikanaropal10@gmail.com>
2023-03-30 12:17:55 -0300

    Transfers the logic of draw image on canvas from useEffect to onClick of download button, removes the onload method and checks if the layers are visible or no for draw.

--- src/Components/ExportPNGButton.js
+++ src/Components/ExportPNGButton.js
@@ -21,22 +21,6 @@ const ExportPNGButton = () => {
   const canvas = document.querySelector("#canvas");
   let ctx = canvas?.getContext("2d");
 
-  React.useEffect(() => {
-    const tiles = [...map.tiles];
-    tiles?.forEach((tile) => {
-      tile?.layers?.forEach((layer) => {
-        const imageData = new Image();
-        imageData.src = layer;
-        imageData.onload = () => {
-          const x1 = tile.x;
-          const y1 = tile.y;
-          ctx.clearRect((x1 - 1) * 100, (y1 - 1) * 100, 100, 100);
-          ctx?.drawImage?.(imageData, (x1 - 1) * 100, (y1 - 1) * 100, 100, 100);
-        };
-      });
-    });
-  }, [map]);
-
   return (
     <>
       <canvas
@@ -50,10 +34,41 @@ const ExportPNGButton = () => {
           exportPNGButtonClickOn ? "exportPNGButton" : ""
         }`}
         onClick={() => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          const completeMap = { ...map };
+          const usedImages = [];
+          completeMap?.tiles.forEach((tile) => {
+            tile?.layers?.forEach((layer, layersIndex) => {
+              if (completeMap?.layers?.[layersIndex]?.visible) {
+                let tempIndex = 0;
+                if (
+                  !usedImages.some((usedImage, usedImagesIndex) => {
+                    tempIndex = usedImagesIndex;
+                    return usedImage.src === layer;
+                  })
+                ) {
+                  usedImages.push({ image: new Image(), src: layer });
+                  const lastImageIndex = usedImages.length - 1;
+                  usedImages[lastImageIndex].image.src = layer;
+                  usedImages[lastImageIndex].src = layer;
+                  tempIndex = lastImageIndex;
+                }
+                const x1 = tile.x;
+                const y1 = tile.y;
+                ctx?.drawImage?.(
+                  usedImages[tempIndex]?.image,
+                  (x1 - 1) * 100,
+                  (y1 - 1) * 100,
+                  100,
+                  100
+                );
+              }
+            });
+          });
+
           const canvasBASE64 = document
             .querySelector("#canvas")
             .toDataURL("image/jpg");
-
           const toDownload = document.createElement("a");
           toDownload.download = map.title;
           toDownload.href = canvasBASE64;
